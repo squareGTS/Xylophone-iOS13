5ab702cc860855d37f2bee866f582c285c825827
squareGTS <68609996+squareGTS@users.noreply.github.com>
2020-07-31 10:05:37 +0300

    Xylophone
    Using and understanding Apple Documentation

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        //print(sender.currentTitle)
+        playSound(soundName: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        print ("Start")
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2), execute: {
+            print("End")
+            sender.alpha = 1
+        })
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
